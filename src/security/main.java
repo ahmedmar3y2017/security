10bb999e0e869068cfb8171e5302a8dfb0437795
ahmedmar3y2017 <ahmedmar3y108@gmail.com>
2017-11-27 01:35:20 +0200

    init

--- src/security/main.java
+++ src/security/main.java
@@ -41,7 +41,51 @@ public class main {
 
 	}
 
-	public static void hillcypher3_enc() {
+	public static void hillcypher3_enc(String plain) {
+		// delete all white spaces
+		String plain_ = plain.replaceAll("\\s", "");
+		// number of letters
+		int n = letter.length();
+		// final cypher
+		StringBuilder cypher = new StringBuilder();
+
+		// init matrix 3*3 as akey
+		int[][] key = new int[3][3];
+		key[0][0] = 1;
+		key[0][1] = 3;
+		key[0][1] = 1;
+
+		key[1][0] = 1;
+		key[1][1] = 1;
+		key[1][1] = 2;
+
+		key[2][0] = 2;
+		key[2][1] = 3;
+		key[2][1] = 4;
+
+		// mul main row
+		int mul1 = 1;
+		// mul second row
+		int mul2 = 1;
+		// value matrix 2*2
+		// find matrix value
+		for (int i = 0; i < key.length; i++) {
+
+			for (int j = 0; j < key.length; j++) {
+				// main row
+				if (i == j) {
+
+					mul1 *= key[i][j];
+
+				}
+				// second row
+				else {
+					mul2 *= key[i][j];
+				}
+
+			}
+
+		}
 
 	}
 
